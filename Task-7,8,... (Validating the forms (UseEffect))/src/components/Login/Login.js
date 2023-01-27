3b3eff62d4b857b01fbe62e7920b0c7bc17175b3
MilanTibadiya <tibadiyamilan@gmail.com>
2023-01-27 09:41:49 +0530

    Done

--- Task-7,8,... (Validating the forms (UseEffect))/src/components/Login/Login.js
+++ Task-7,8,... (Validating the forms (UseEffect))/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -44,6 +44,9 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -94,6 +97,13 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+    if(formIsValid){
+      authCtx.onLogin(emailState.value, passwordState.value);
+    }else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    }else{
+      passwordInputRef.current.focus();
+    }
     authCtx.onLogin(emailState.value, passwordState.value);
   };
 
@@ -101,6 +111,7 @@ const Login = (props) => {
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -110,6 +121,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -120,7 +132,7 @@ const Login = (props) => {
         />
 
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
